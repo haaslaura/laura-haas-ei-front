2371386ac78ba6bbd3dd96b3a582db29a989127b
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-07-07 18:02:10 +0200

    Création d'un composant call-to-action

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import CallToAction from '../components/CallToAction';
 import CardSection from '../components/CardSection';
 import Hero from '../components/Hero';
+import OffersSection from '../components/OffersSection';
 import StageSection from '../components/StageSection';
+import TestimonialSection from '../components/TestimonialSection';
 
 const Home = () => {
     return (
@@ -8,6 +11,26 @@ const Home = () => {
             <Hero />
             <CardSection />
             <StageSection />
+            <OffersSection />
+            <TestimonialSection />
+            <CallToAction
+                title={"Prêt à donner vie à votre projet\u00A0?"}
+                bigTitle={true}
+                subtitle={"Contactez-moi pour un échange sans engagement. Le café est pour moi (même en visio)\u00A0!"}
+                linkContent="Contactez-moi"
+                link="/contact"
+                linkAsButton={true}
+                separation={true}
+            />
+            <CallToAction
+                title={"Vous êtes une agence\u00A0?"}
+                bigTitle={false}
+                subtitle="Je collabore en marque blanche et propose des renforts fiables en développement front-end."
+                linkContent="Contactez-moi"
+                link="/contact"
+                linkAsButton={false}
+                separation={false}
+            />
         </section>
     );
 };
