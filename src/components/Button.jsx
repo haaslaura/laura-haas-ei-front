06cb52a7f7eab7a958b4e69cb7a9888c91878c2e
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-07-30 20:43:51 +0200

    Improved button to open the link in a new tab

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 
-const Button = ({ text, link }) => {
+const Button = ({ text, link, blank = false }) => {
+    const linkProps = blank ? { target: '_blank', rel: 'noopener noreferrer' } : {};
+
     return (
         <Link
             to={link}
-            className="bg-[var(--color-accent)] text-[var(--color-dark-blue)] font-bold py-2 px-4 rounded-lg hover:opacity-90 transition-all whitespace-nowrap"
+            {...linkProps}
+            className="bg-(--color-accent) text-(--color-dark-blue) font-bold py-2 px-4 rounded-lg hover:opacity-90 transition-all whitespace-nowrap"
         >
             {text}
         </Link>
