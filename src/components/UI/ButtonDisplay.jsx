2f1907fd5485d79e27bf67de802e3920a1b0969c
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-09-17 16:47:42 +0200

    Animation correction

--- src/components/UI/ButtonDisplay.jsx
+++ src/components/UI/ButtonDisplay.jsx
@@ -2,7 +2,7 @@ const ButtonDisplay = ({ text, buttonAction }) => {
     return (
         <button
             onClick={buttonAction}
-            className="cursor-pointer bg-(--color-accent) text-(--color-dark-blue) font-bold py-2 px-4 rounded-lg hover:opacity-90 transition-all whitespace-nowrap transition-transform duration-300 ease-out hover:-translate-y-1"
+            className="cursor-pointer bg-(--color-accent) text-(--color-dark-blue) font-bold py-2 px-4 rounded-lg hover:opacity-90 transition-all whitespace-nowrap duration-300 ease-out hover:-translate-y-1"
         >
             {text}
         </button>
